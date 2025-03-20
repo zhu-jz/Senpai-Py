cd1db1559863897a332501d46dd322707bdd3271
zhu-jz <zhu.jianzhao1984@gmail.com>
2025-03-20 18:58:07 +0800

    Update copyright information

--- senpai_10.py
+++ senpai_10.py
@@ -1,8 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Nov 27 16:29:14 2024
+Senpai 1.0
 
-@author: ZHU
+Copyright (C) 2014 Fabien Letouzey.
+Copyright (C) 2025 Jianzhao Zhu (translation to Python).
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __future__ import annotations
